6aab2f68fe2d400507021893a4426d21871787e7
CYC <s24343162@gmail.com>
2024-06-17 01:00:17 +0800

    1985H2

--- Round952_1985/1985H2.cpp
+++ Round952_1985/1985H2.cpp
@@ -3,20 +3,17 @@ using namespace std;
 using ll = long long;
 using unll = unsigned long long;
  
-vector<set<int>> component_i, component_j;
-vector<int> N;
- 
-void dfs(vector<vector<char>>& g, int i, int j, set<int>& I, set<int>& J, int& num) {
+void dfs(vector<vector<char>>& g, ll i, ll j, set<ll>& I, set<ll>& J, ll& num) {
     if(g[i][j] == '.') return;
     g[i][j] = '.';
     num++;
     
-    I.insert(i-1);
+    if(i-1 >= 0) I.insert(i-1);
     I.insert(i);
-    I.insert(i+1);
-    J.insert(j-1);
+    if(i+1 < g.size()) I.insert(i+1);
+    if(j-1 >= 0) J.insert(j-1);
     J.insert(j);
-    J.insert(j+1);
+    if(j+1 < g[0].size()) J.insert(j+1);
  
     if(i+1<g.size() && g[i+1][j] == '#')    dfs(g, i+1, j, I, J, num);
     if(j+1<g[0].size() && g[i][j+1] == '#') dfs(g, i, j+1, I, J, num);
@@ -25,15 +22,12 @@ void dfs(vector<vector<char>>& g, int i, int j, set<int>& I, set<int>& J, int& n
 }
  
 void sol() {
-    component_i.clear();
-    component_j.clear();
-    N.clear();
- 
-    int n, m;
+    ll n, m, res = 0, cnt = 0;
     cin >> n >> m;
     vector<vector<char>> g(n, vector<char>(m, '$'));
  
-    vector<int> dotI(n, 0), dotJ(m, 0);
+    vector<ll> dotI(n, 0), dotJ(m, 0);
+    vector<vector<ll>> com(n, vector<ll>(m, 0));
  
     for(int i=0; i<n; i++) {
         for(int j=0; j<m; j++) {
@@ -41,61 +35,42 @@ void sol() {
             if(g[i][j] == '.') {
                 dotI[i]++;
                 dotJ[j]++;
+                //com[i][j] = -1;
+                cnt++;
             }
         }
     }
- 
+    if(cnt == n * m) {
+        cout << n + m - 1 << endl;
+        return;
+    }
+
     vector<vector<char>> temp(g);
     for(int i=0; i<n; i++) {
         for(int j=0; j<m; j++) {
             if(temp[i][j] == '.') continue;
-            set<int> I, J;
-            int num = 0;
+            set<ll> I, J;
+            ll num = 0;
             dfs(temp, i, j, I, J, num);
-            component_i.push_back(I);
-            component_j.push_back(J);
-            N.push_back(num);
- 
-            for(auto& ii : I) if(ii>=0 && ii<n) dotI[ii]+=num;
-            for(auto& jj : J) if(jj>=0 && jj<m) dotJ[jj]+=num;
+            
+            for(auto& ii : I) dotI[ii] += num;
+            for(auto& jj : J) dotJ[jj] += num;
+           
+            for(auto& ii : I) {
+                for(auto& jj : J) {
+                    com[ii][jj] += num;
+                }
+            }
         }
     }
-    //cout << component_i.size() << ' ' << component_j.size() << ' ' << N.size() << endl;
- 
-    /*
-    int res = 0, sz = component_i.size();
+    
     for(int i=0; i<n; i++) {
-        int cnt = 0;
-        for(int c=0; c<sz; c++) {
-            if(component_i[c].count(i)) cnt += N[c];
-        }
-        //for(int j=0; j<m; j++) {
-        //    if(g[i][j] == '.') cnt++;
-        //}
-        cnt += dotI[i];
-        res = max(res, cnt);
-    }
-    for(int j=0; j<m; j++) {
-        int cnt = 0;
-        for(int c=0; c<sz; c++) {
-            if(component_j[c].count(j)) cnt += N[c];
-        }
-        //for(int i=0; i<n; i++) {
-        //    if(g[i][j] == '.') cnt++;
-        //}
-        cnt += dotJ[j];
-        res = max(res, cnt);
-    }
-    */
- 
-    int res = 0;
-    //for(auto& ii : dotI) res = max(res, ii);
-    //for(auto& jj : dotJ) res = max(res, jj);
-    for(auto&  ii :dotI) {
-         for(auto& jj : dotJ) {
-            res = max (res, ii +  jj);
+        for(int j=0; j<m; j++) {
+            if(g[i][j] == '.') res = max(res, dotI[i] + dotJ[j] - com[i][j] - 1);
+            else res = max(res, dotI[i] + dotJ[j] - com[i][j]);
         }
     }
+
     cout << res << endl;
 }
  
