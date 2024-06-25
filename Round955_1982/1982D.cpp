03cccfe0fe7b52cb4353f0c61627410ecc5e6ca1
CYC <s24343162@gmail.com>
2024-06-25 22:16:56 +0800

    round955

---
+++ Round955_1982/1982D.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using unll = unsigned long long;
+
+void sol() {
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int test; cin >> test;
+	for(int i=1; i<=test; i++) {
+		sol();
+	}
+	return 0;
+}
+
