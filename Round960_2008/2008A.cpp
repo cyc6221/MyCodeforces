236516f415f83563a1e2b9211e50e4b05aafb47d
CYC <s24343162@gmail.com>
2024-09-01 21:46:17 +0800

    2008

---
+++ Round960_2008/2008A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using unll = unsigned long long;
+
+void sol() {
+    string s1, s2;
+    cin >> s1 >> s2;
+    cout << s1 + s2 << endl;
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int test; cin >> test;
+	for(int i=1; i<=test; i++) {
+		sol();
+	}
+	return 0;
+}
+
