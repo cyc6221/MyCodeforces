8aea4ee0be81199e14ea79f7f129079d36469a0d
CYC <s24343162@gmail.com>
2024-10-02 17:45:49 +0800

    1971F

--- Round944_1971p/1971F.cpp
+++ Round944_1971p/1971F.cpp
@@ -4,6 +4,25 @@ using ll = long long;
 using unll = unsigned long long;
 
 void sol() {
+	ll r;
+	cin >> r;
+
+	ll res = 0;
+	ll x, y, pre = 0;
+
+	for(x = r; x > 0; x--) {
+		for(y = pre; y <= r; y++) {
+			if(x * x + y * y >= r * r && x * x + y * y < r * r + 2 * r + 1) break;
+			else pre++;
+		}
+		//cout << pre << endl;
+		for(y = pre; y <= r; y++) {
+			if(x * x + y * y >= r * r && x * x + y * y < r * r + 2 * r + 1) res++;
+			else break;
+		}
+	}
+
+	cout << res * 4 << endl;
 }
 
 int main() {
